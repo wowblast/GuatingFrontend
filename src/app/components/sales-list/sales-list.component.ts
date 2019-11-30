5af52f80383656caca6d85d23dd7026ff60d685d
javivi235 <javisoruco235@gmail.com>
2019-11-30 14:08:59 -0400

    minor fix

--- src/app/components/sales-list/sales-list.component.ts
+++ src/app/components/sales-list/sales-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SalesListComponent implements OnInit {
 
-  @Input() quotes: Array<Object>;
+  @Input() quotes: Array<any>;
   private total = [];
   constructor() { }
 
